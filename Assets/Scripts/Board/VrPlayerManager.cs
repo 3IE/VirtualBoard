6671d7f8ecb595024891f41bbda9f3a34e207a7b
ScareFire <noe.topeza@epita.fr>
2022-09-15 15:28:54 +0200

    Event ping added, now sends its own position on the network correctly

--- Assets/Scripts/Board/VrPlayerManager.cs
+++ Assets/Scripts/Board/VrPlayerManager.cs
@@ -7,22 +7,30 @@ public class VrPlayerManager : MonoBehaviour
 {
     // a attacher au XR Origin/Camera Offset
     private Transform VRCamTransform;
-    
-    [SerializeField] private float _refreshRate = 0.5f;
+    [SerializeField] private Transform BoardTransform;
+    [SerializeField] private float _refreshRate = 0.2f;
 
     private void OnEnable()
     {
         VRCamTransform = GetComponentInChildren<Camera>().transform;
-        //InvokeRepeating(nameof(SendNewPositionEvent), _refreshRate, _refreshRate);
+        InvokeRepeating(nameof(SendNewPositionEvent), _refreshRate, _refreshRate);
     }
     private void OnDisable() => CancelInvoke();
 
     private void SendNewPositionEvent()
     {
-        object[] content = { VRCamTransform.position };
+        object[] content = { VRCamTransform.position - BoardTransform.position };
         
         var raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.Others };
 
         PhotonNetwork.RaiseEvent((byte) Event.EventCode.SendNewPosition, content, raiseEventOptions, SendOptions.SendReliable);
+    }
+    
+    //! TO DELETE LATER
+    public void HahaPinged()
+    {
+        object[] content = { new Vector2(1, 1)};
+        var raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.Others };
+        PhotonNetwork.RaiseEvent((byte) Event.EventCode.SendNewPing, content, raiseEventOptions, SendOptions.SendReliable);
     }
 }
