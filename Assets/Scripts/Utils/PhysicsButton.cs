332d00df9568d5eb149d522aace76b76df069c24
Yvon Morice <yvon.morice5@gmail.com>
2022-10-17 11:38:36 +0200

    added buttons to reset the board and the shapes

---
+++ Assets/Scripts/Utils/PhysicsButton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using Random = UnityEngine.Random;
+
+namespace Utils
+{
+    public class PhysicsButton : MonoBehaviour
+    {
+        [SerializeField] private Collider bodyButtonCollider;
+        [SerializeField] private Collider buttonCollider;
+        
+        [SerializeField] private AudioSource pressedSound;
+        [SerializeField] private AudioSource releasedSound;
+        [SerializeField] private UnityEvent  onPressed;
+        [SerializeField] private Animator    animator;
+
+        private static readonly int Pushed = Animator.StringToHash("pushed");
+
+        private void Awake()
+        {
+            Physics.IgnoreCollision(buttonCollider, bodyButtonCollider);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            animator.SetBool(Pushed, true);
+            Pressed();
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            animator.SetBool(Pushed, false);
+            Released();
+        }
+
+        private void Pressed()
+        {
+            pressedSound.pitch = 1;
+            pressedSound.Play();
+            onPressed.Invoke();
+        }
+
+        private void Released()
+        {
+            releasedSound.pitch = Random.Range(1.1f, 1.2f);
+            releasedSound.Play();
+        }
+    }
+}
